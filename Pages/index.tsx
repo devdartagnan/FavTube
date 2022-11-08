cfa2a5ad9796f3780574933973b19991f796e471
Leozinsk <leozinsk@gmail.com>
2022-11-08 18:43:04 +0000

    Update

--- Pages/index.tsx
+++ Pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Menu from '../Styles/Homepage/Menu.js'
-import Header from '../Components/Header.js'
-import Timeline from '../Components/Timeline.js'
+import Header from '../Components/Header'
+import Timeline from '../Components/Timeline'
+import Profiles from '../Components/Favprofiles'
 import { CSSReset } from '../Styles/CSSReset'
 import Home from '../Styles/Homepage/Home.js'
-import Profiles from '../Components/Favprofiles'
 
 export default function HomePage() {
     return (
